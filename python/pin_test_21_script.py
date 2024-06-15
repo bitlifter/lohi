bb43dc250ad2c4886de091513f535f7496c2898d
Chris Edgar <8068808+bitlifter@users.noreply.github.com>
2024-06-14 19:42:06 -0600

    changed to pin 6 for pin_test_21_script

--- python/pin_test_21_script.py
+++ python/pin_test_21_script.py
@@ -4,15 +4,15 @@ import time
 # Use GPIO numbers not pin numbers
 GPIO.setmode(GPIO.BCM)
 
-# set up the GPIO channels - one input (GPIO 21)
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# set up the GPIO channels - one input (GPIO 6)
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def test_connectivity():
-    # Check the status of pin 21
-    if GPIO.input(21):
-        print("Pin 21 is HIGH")
+    # Check the status of pin 6
+    if GPIO.input(6):
+        print("Pin 6 is HIGH")
     else:
-        print("Pin 21 is LOW")
+        print("Pin 6 is LOW")
 
 # Test connectivity
 try:
